829a48dc4f4dd5f02cca97b4feb3d2f2d8b12cab
Deepak Kumar Singh <deepak.singh@research.iiit.ac.in>
2019-06-20 11:24:39 +0530

    updated oops_general and removed unnecessary files

--- python_course_eu/oops/general.py
+++ python_course_eu/oops/general.py
@@ -71,3 +71,78 @@ and their corresponding values
 '''
 x.__dict__
 y.__dict__
+
+
+'''Attributes can be bound to class names as well, in this case, each instance will possess
+this name as well. But we should be aware when we assign the same name to an instance.'''
+
+class Robot1(object):
+    pass
+
+x = Robot1()
+Robot1.brand = 'kuka'
+x.brand
+
+x.brand = 'Honda'
+
+Robot1.brand
+x.brand
+y = Robot1()
+y.brand
+
+Robot1.brand = 'Thales'
+y.brand # when we try to access y.brand, python first checks for 'brand' in y.__dict__, if it is
+        # not found then it will for Robot1.__dict__. If it is not found even in the className.__dict__
+        # then AttributeError will be raised.
+x.brand
+
+'''So the class attributes will be accessible from all instances and when we reassign
+the same attribute with a new value for an instance, the instance will bound the attribut
+for itself'''
+
+x.__dict__
+y.__dict__
+
+Robot1.__dict__
+
+x.energy # This will raise AttributeError since the attribute 'energy' is neither in x.__dict___
+        # nor in ClassName.__dict__. Here the ClassName is Robot1.
+
+getattr(x,'energy','No_Attr_energy')
+getattr(x,'brand','No_Brand')
+
+''' Even function names can be attributed. We can bind an attribute to a function name in the same way
+we have done so far to othe instances of classes. '''
+
+def f(x):
+    '''function doc string goes here'''
+    return 42
+
+f.y = 100
+print(f.y)
+
+dir(f)
+
+for attr_name in dir(f):
+    print('attr_name:{} \t value:{}'.format(attr_name, getattr(f,attr_name,"no_value")))
+
+
+f.__doc__
+
+def f(x):
+    f.counter = getattr(f, "counter", 0 ) + 1
+    if f.counter % 2 == 0:
+        f.isCalledEvenTimes = True
+    else:
+        f.isCalledEvenTimes = False
+    return "Monty Python"
+
+for i in range(10):
+    f(i)
+
+print(f.counter)
+print(f.isCalledEvenTimes) 
+
+# using the above function attributes technique we can save function related values within the function.
+# use cases: function call counter, decision based on number of function calls, maybe on who called the function.
+
