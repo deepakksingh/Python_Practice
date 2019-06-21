f04780c613168949eb7baaadf0b3467893537570
Deepak Kumar Singh <deepak.singh@research.iiit.ac.in>
2019-06-21 12:45:04 +0530

    added class methods

--- python_course_eu/oops/class_and_instance_attributes.py
+++ python_course_eu/oops/class_and_instance_attributes.py
@@ -143,4 +143,65 @@ if __name__ == "__main__":
     print(y.RobotInstances())
     print(Robot.RobotInstances())
 
+# Class Methods
+'''
+Static methods shouldn't be confused with class methods.
+Like static methods class methods are not bound to instances, but unlike static methods class
+methods are bound to a class.
+i.e. a class object.
+Class methods can be called via an instance or the class name
+'''
 
+class Robot:
+    'class definition of the class Robot'
+
+    __counter = 0
+
+    def __init__(self):
+        'initializer method'
+        # print(type(self))
+        type(self).__counter += 1
+
+    @classmethod
+    def RobotInstances(cls):
+        return cls, Robot.__counter
+
+if __name__ == "__main__":
+    print(Robot.RobotInstances())
+    x = Robot()
+    print(x.RobotInstances())
+    y = Robot()
+    print(y.RobotInstances())
+    print(Robot.RobotInstances())
+
+
+'''
+Use cases of class methods:
+1.  Factory methods.
+2.  They are often used, where we have static methods, which have to call other static methods.
+    To do this, we would have to hard code the class name, if we had to use static methods.
+    This is a problem, if we are in a use case, where we have inherited classes.
+'''
+
+class Fraction(object):
+    'class definition of the class Fraction'
+
+    def __init__(self,n,d):
+        self.numerator, self.denominator = Fraction.reduce(n, d)
+
+    @staticmethod
+    def gcd(a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
+    
+    @classmethod
+    def reduce(cls, n1, n2):
+        g = cls.gcd(n1, n2)
+        return (n1 // g, n2 // g)
+
+    def __str__(self):
+        return str(self.numerator) + '/' + str(self.denominator)
+
+x = Fraction(8,24)
+print(x)
